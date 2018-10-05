019097ae9cf6544fce4f83cd07e2a086bd7b809d
yonashail <yodevhail@gmail.com>
2018-10-05 14:20:02 -0400

    First Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,27 +6,30 @@ public class Main {
 
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        String newnumber=("n");
+        String newnumber=("y");
 // Calculate the multiplication of 5 and 7 of any number using do-while and for loop
-    {do {
-        int n;
-        System.out.println("Enter an integer");
+        {do {
+            int n;
+            System.out.println("Enter an integer");
 
-        n = in.nextInt();
+            n = in.nextInt();
 
 
-        for (int counter = n; counter >= 1; counter--) {
-            if (counter % 5 == 0) {
-                System.out.println(counter);
-                // write your code here
-            } else if (counter % 7 == 0) {
-                System.out.println(counter);
+            for (int counter = n; counter >= 1; counter--) {
+                if (counter % 5 == 0) {
+                    System.out.println(counter);
+                    // write your code here
+                } else if (counter % 7 == 0) {
+                    System.out.println(counter);
+                }
             }
-        }
-        System.out.println("Would you like to enter another number?");
-        newnumber = in.nextLine();
-    }while (newnumber.equals("y"));}
-    System.out.println("Thank you for playing!!");
+            System.out.println("Would you like to enter another number,If yes enter Y and if no enter N?");
+            newnumber = in.nextLine();
+        }while (newnumber.equalsIgnoreCase("n"));}
+        System.out.println("Thank you for playing!!");
     }
 
 }
+
+
+
